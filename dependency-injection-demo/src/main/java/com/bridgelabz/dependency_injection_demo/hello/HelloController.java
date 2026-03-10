87e4795cbab3399c54b140d439b9c4f5e9f059ce
Sneha <snehangap@gmail.com>
2026-03-10 15:51:21 +0530

    [Sneha] : UC2 : Added query parameter hello API

--- dependency-injection-demo/src/main/java/com/bridgelabz/dependency_injection_demo/hello/HelloController.java
+++ dependency-injection-demo/src/main/java/com/bridgelabz/dependency_injection_demo/hello/HelloController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.dependency_injection_demo.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
